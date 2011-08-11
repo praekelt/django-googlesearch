39b4544eed5b13fd15475df7dd7374181a96c198
Shaun Sephton <shaunsephton@gmail.com>
2011-08-11 15:28:25 +0200

    prepare v0.0.5 release

---
+++ CHANGELOG.rst
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+0.0.5 (2011-08-11)
+------------------
+#. Refactor to not use django-preferences.
+#. Documentation.
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-googlesearch',
-    version='0.0.4',
+    version='0.0.5',
     description='Django Google custom search engine app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
