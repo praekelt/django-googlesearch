57d057e59c64d986bd148c8611b89c52c712bd45
Shaun Sephton <shaunsephton@gmail.com>
2011-08-30 12:59:32 +0200

    pep8

--- googlesearch/templatetags/googlesearch_inclusion_tags.py
+++ googlesearch/templatetags/googlesearch_inclusion_tags.py
@@ -3,13 +3,17 @@ from django.conf import settings
 
 register = template.Library()
 
-@register.inclusion_tag('googlesearch/inclusion_tags/googlesearch_input.html', takes_context=True)
+
+@register.inclusion_tag('googlesearch/inclusion_tags/googlesearch_input.html',\
+        takes_context=True)
 def googlesearch_input(context):
     context.update({
         'google_search_partner_id': settings.GOOGLE_SEARCH_PARTNER_ID
     })
     return context
 
-@register.inclusion_tag('googlesearch/inclusion_tags/googlesearch_results.html')
+
+@register.inclusion_tag('googlesearch/inclusion_tags/\
+        googlesearch_results.html')
 def googlesearch_results():
     return {}
--- googlesearch/urls.py
+++ googlesearch/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'django.views.generic.simple',
-    url(r'^results/$', 'direct_to_template', {'template': 'googlesearch/googlesearch_results.html', 'extra_context': {'title' : 'Search Results'}}, name='googlesearch_results'),
+    url(r'^results/$', 'direct_to_template', {'template': \
+            'googlesearch/googlesearch_results.html', 'extra_context': \
+            {'title': 'Search Results'}}, name='googlesearch_results'),
 )
