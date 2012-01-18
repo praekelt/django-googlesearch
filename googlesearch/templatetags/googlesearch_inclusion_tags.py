e5224bb40ff4869f11e106e60faa63634a1ddf15
Mitar <mitar@tnode.com>
2012-01-18 10:51:08 +0100

    Fixing template tags' inclusion template names/paths.

--- googlesearch/templatetags/googlesearch_inclusion_tags.py
+++ googlesearch/templatetags/googlesearch_inclusion_tags.py
@@ -4,8 +4,7 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.inclusion_tag('googlesearch/inclusion_tags/googlesearch_input.html',\
-        takes_context=True)
+@register.inclusion_tag('googlesearch/inclusion_tags/googlesearch_input.html', takes_context=True)
 def googlesearch_input(context):
     context.update({
         'google_search_partner_id': settings.GOOGLE_SEARCH_PARTNER_ID
@@ -13,7 +12,6 @@ def googlesearch_input(context):
     return context
 
 
-@register.inclusion_tag('googlesearch/inclusion_tags/\
-        googlesearch_results.html')
+@register.inclusion_tag('googlesearch/inclusion_tags/googlesearch_results.html')
 def googlesearch_results():
     return {}
